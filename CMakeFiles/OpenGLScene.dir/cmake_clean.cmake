0cf578cabd86beb6c0dc263d1293f52b9667acc3
franslit <frans.littow@gmail.com>
2025-01-12 18:14:53 +0200

    init

---
+++ CMakeFiles/OpenGLScene.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLScene.dir/external/glad/src/glad.c.o"
+  "CMakeFiles/OpenGLScene.dir/external/glad/src/glad.c.o.d"
+  "CMakeFiles/OpenGLScene.dir/main.cpp.o"
+  "CMakeFiles/OpenGLScene.dir/main.cpp.o.d"
+  "OpenGLScene"
+  "OpenGLScene.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLScene.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/OpenGLScene.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OpenGLScene.
+# This may be replaced when dependencies are built.
